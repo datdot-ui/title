92af73af7956aa0a5122f3fdcfeadf72dd712648
Nina Breznik <ninabreznik@gmail.com>
2022-02-12 01:15:30 +0100

    Add logs and protocol

--- demo/demo.js
+++ demo/demo.js
@@ -2,17 +2,43 @@ const bel = require('bel')
 const csjs = require('csjs-inject')
 const title = require('..')
 const main_title = require('./title.json')
+const message_maker = require('message-maker')
+const logs = require('datdot-ui-logs')
 
-function demoComponent() {
-    const defaultTitle = {
+var id = 0
+
+function demo() {
+// ---------------------------------------------------------------
+    const myaddress = `demo-${id++}`
+    const inbox = {}
+    const outbox = {}
+    const recipients = {}
+    const message_id = to => ( outbox[to] = 1 + (outbox[to]||0) )
+
+    function make_protocol (name) {
+        return function protocol (address, notify) {
+            recipients[name] = { address, notify, make: message_maker(myaddress) }
+            return { notify: listen, address: myaddress }
+        }
+    }
+    function listen (msg) {
+        console.log('New message', { msg })
+        const { head, refs, type, data, meta } = msg // receive msg
+        inbox[head.join('/')] = msg                  // store msg
+        const [from] = head
+        recipients['logs'].notify(msg)
+    }
+// ---------------------------------------------------------------
+    const log_list = logs(make_protocol('logs'))
+    const defaultTitle = title({
         page: 'demo',
         name: 'title',
         content: 'Demo title',
         theme: {
             ...main_title,
         }
-    }
-    const title1 = {
+    }, make_protocol('title-demo'))
+    const title1 = title({
         page: 'demo',
         name: 'title',
         content: 'Create new account',
@@ -20,9 +46,9 @@ function demoComponent() {
             ...main_title,
             color: 'var(--color-violet-color-wheel)'
         }
-    }
+    }, make_protocol('title-1'))
 
-    const title2= {
+    const title2= title({
         page: 'demo',
         name: 'title',
         content: 'Import account',
@@ -31,29 +57,18 @@ function demoComponent() {
             color: 'var(--color-sring-green)',
             padding: '20px 0'
         }
-    }
-    const content = bel`
-    <div class=${css.content}>
-        ${title(defaultTitle)}
-        ${title(title1)}
-        ${title(title2)}
-    </div>`
+    }, make_protocol('title-2'))
 
-    // container
-    const container = wrap(content)
-    return container
-
-    function wrap (content, terminal) {
-        const container = bel`
+    const el = bel`
         <div class=${css.wrap}>
             <section class=${css.container}>
-                ${content}
+                <div class=${css.content}> ${defaultTitle} ${title1} ${title2} </div>
             </section>
-            ${terminal}
-        </div>
-        `
-        return container
-    }
+            ${log_list}
+        </div>`
+    
+    console.log({el})
+    return el
 
 }
 
@@ -64,12 +79,70 @@ const css = csjs`
     --font-light: 100;
     --font-normal: 300;
     --font-bold: 600;
+    --color-dark: 223, 13%, 20%;
     --color-black: hsl(0, 0%, 0%);
     --color-white: hsl(0, 0%, 100%);
     --color-orange: hsl(38, 100%, 50%);
     --color-blue: hsl(246, 100%, 50%);
     --color-sring-green: hsl(246, 100%, 50%);
     --color-violet-color-wheel: hsl(268, 100%, 50%);
+    --color-white: var(--b), 100%;
+    --color-black: var(--b), 0%;
+    --color-dark: 223, 13%, 20%;
+    --color-deep-black: 222, 18%, 11%;
+    --color-blue: 214, var(--r);
+    --color-red: 358, 99%, 53%;
+    --color-amaranth-pink: 331, 86%, 78%;
+    --color-persian-rose: 323, 100%, 56%;
+    --color-orange: 35, 100%, 58%;
+    --color-deep-saffron: 31, 100%, 56%;
+    --color-ultra-red: 348, 96%, 71%;
+    --color-flame: 15, 80%, 50%;
+    --color-verdigris: 180, 54%, 43%;
+    --color-maya-blue: 205, 96%, 72%;
+    --color-slate-blue: 248, 56%, 59%;
+    --color-blue-jeans: 204, 96%, 61%;
+    --color-dodger-blue: 213, 90%, 59%;
+    --color-light-green: 127, 86%, 77%;
+    --color-lime-green: 127, 100%, 40%;
+    --color-slimy-green: 108, 100%, 28%;
+    --color-maximum-blue-green: 180, 54%, 51%;
+    --color-green: 136, 81%, 34%;
+    --color-light-green: 97, 86%, 77%;
+    --color-lincoln-green: 97, 100%, 18%;
+    --color-yellow: 44, 100%, 55%;
+    --color-chrome-yellow: 39, var(--r);
+    --color-bright-yellow-crayola: 35, 100%, 58%;
+    --color-green-yellow-crayola: 51, 100%, 83%;
+    --color-purple: 283, var(--r);
+    --color-medium-purple: 269, 100%, 70%;
+    --color-grey33: var(--b), 20%;
+    --color-grey66: var(--b), 40%;
+    --color-grey70: var(--b), 44%;
+    --color-grey88: var(--b), 53%;
+    --color-greyA2: var(--b), 64%;
+    --color-greyC3: var(--b), 76%;
+    --color-greyCB: var(--b), 80%;
+    --color-greyD8: var(--b), 85%;
+    --color-greyD9: var(--b), 85%;
+    --color-greyE2: var(--b), 89%;
+    --color-greyEB: var(--b), 92%;
+    --color-greyED: var(--b), 93%;
+    --color-greyEF: var(--b), 94%;
+    --color-greyF2: var(--b), 95%;
+    --size12: 1.2rem;
+    --size14: 1.4rem;
+    --size16: 1.6rem;
+    --size18: 1.8rem;
+    --size20: 2rem;
+    --size22: 2.2rem;
+    --size24: 2.4rem;
+    --size26: 2.6rem;
+    --size28: 2.8rem;
+    --size30: 3rem;
+    --size32: 3.2rem;
+    --size36: 3.6rem;
+    --size40: 4rem;
 }
 html {
     box-sizing: border-box;
@@ -97,7 +170,29 @@ body {
     overflow-y: auto;
 }
 .content {}
-.content 
+[data-state="view"] i-log {
+    display: none;
+}
+[data-state="debug"] i-log {
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 40%;
+    height: 100%;
+}
+@media (max-width: 768px) {
+    [data-state="debug"] {
+        grid-template-rows: 65% 35%;
+        grid-template-columns: auto;
+    }
+    [data-state="debug"] i-log {
+        position: inherit;
+        width: 100%;
+    }
+    .container {
+        grid-template-rows: 80px auto;
+    }
+}
 `
 
-document.body.append( demoComponent() )
+document.body.append(demo())
--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 const bel = require('bel')
 const csjs = require('csjs-inject')
+const message_maker = require('message-maker')
+
+var id = 0
 
 module.exports = title 
-function title(option) {
-    const {page, flow, name, content, theme} = option
+
+function title(opts, parent_protocol) {
+    console.log({opts, parent_protocol})
+// ---------------------------------------------------------------
+    const myaddress = `title-${id++}`
+    const inbox = {}
+    const outbox = {}
+    const recipients = {}
+    const message_id = to => ( outbox[to] = 1 + (outbox[to]||0) )
+
+    const {notify, address} = parent_protocol(myaddress, listen)
+    const make = message_maker(myaddress)
+    let message = make({ to: address, type: 'ready', refs: ['old_logs', 'new_logs'] })
+    notify(message)
+
+    function listen (msg) {
+
+    }
+// ---------------------------------------------------------------
+    const {page, flow, name, content, theme} = opts
 
     function ui_element(css) {
         const widget = 'ui-title'
